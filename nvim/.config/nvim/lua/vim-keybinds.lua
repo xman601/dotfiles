6d1f54e825209ee08afc9b167bb001f16acd2a84
cyber <cyber610@pm.me>
2025-04-23 14:17:32 -0700

    nvim change

--- nvim/.config/nvim/lua/vim-keybinds.lua
+++ nvim/.config/nvim/lua/vim-keybinds.lua
@@ -1,5 +1,5 @@
 vim.keymap.set('n', '<leader>q', ':q!<CR>')
-vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>w', ':w!<CR>')
 vim.keymap.set('n', '<C-c>', ':!')
 
 -- Noice keymaps
